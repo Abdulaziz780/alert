9370215841b34db27708dc9741d71237205cb72b
Abdulaziz780 <akhusanov410@gmail.com>
2024-03-14 13:50:02 +0500

    oddi

---
+++ main.js
@@ -0,0 +1,5 @@
+let num1 = prompt('ismingizni kiriting')
+let num2 = prompt('familiyani kiriting')
+let num3 = prompt('yoshingizni kiriting')
+
+alert(`ismingiz ${num1} \nfamililez ${num2} \nyoshingiz ${num3}`)
